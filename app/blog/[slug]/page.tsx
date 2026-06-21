9b9a35de3ab9615a308ac8d777752790db47b357
Minh20235781 <minh.nn235781@sis.hust.edu.vn>
2026-06-21 09:59:05 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,6 +4,14 @@ type Params = {
   };
 };
 
+export async function generateStaticParams() {
+  return [
+    { slug: "post-1" },
+    { slug: "post-2" },
+    { slug: "hello-world" },
+  ];
+}
+
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
